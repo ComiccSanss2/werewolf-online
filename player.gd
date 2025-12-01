b419acd0fc3d3b7c947822afcb9d6548871b6cf9
Hamza Ben Alouane <114566360+ComiccSanss2@users.noreply.github.com>
2025-12-01 15:14:24 +0100

    Added Camera2D for all players, and spawnpoints

--- player.gd
+++ player.gd
@@ -1,49 +1,52 @@
 extends CharacterBody2D
 
 @export var speed := 75
-@onready var anim = $AnimatedSprite2D
+@onready var anim := $AnimatedSprite2D
 
 var last_direction := Vector2.DOWN
+var network_pos := Vector2.ZERO
+
 
 func _enter_tree():
 	if is_multiplayer_authority():
 		$Camera2D.enabled = true
-
-func _ready():
-	if is_multiplayer_authority():
-		$Camera2D.enabled = true
+		$Camera2D.make_current()
 	else:
 		$Camera2D.enabled = false
 
 
+func _ready():
+	pass
 
 
 func _physics_process(delta):
-	if not is_multiplayer_authority():
-		return
+	var input_dir := Vector2.ZERO
 
-	var input_dir = Vector2.ZERO
+	if is_multiplayer_authority():
+		if Input.is_action_pressed("ui_up"):
+			input_dir.y -= 1
+		if Input.is_action_pressed("ui_down"):
+			input_dir.y += 1
+		if Input.is_action_pressed("ui_left"):
+			input_dir.x -= 1
+		if Input.is_action_pressed("ui_right"):
+			input_dir.x += 1
 
-	if Input.is_action_pressed("ui_up"):
-		input_dir.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		input_dir.y += 1
-	if Input.is_action_pressed("ui_left"):
-		input_dir.x -= 1
-	if Input.is_action_pressed("ui_right"):
-		input_dir.x += 1
+		input_dir = input_dir.normalized()
 
-	input_dir = input_dir.normalized()
+		if input_dir != Vector2.ZERO:
+			last_direction = input_dir
 
-	if input_dir != Vector2.ZERO:
-		last_direction = input_dir
+		velocity = input_dir * speed
+		move_and_slide()
 
-	velocity = input_dir * speed
-	move_and_slide()
+		_update_animation(input_dir)
 
-	_update_animation(input_dir)
+		rpc("sync_direction", input_dir, last_direction)
+		rpc("sync_position", global_position)
+	else:
+		global_position = global_position.lerp(network_pos, 0.25)
 
-	rpc("sync_position", global_position)
 
 
 func _update_animation(dir: Vector2):
@@ -52,22 +55,23 @@ func _update_animation(dir: Vector2):
 			anim.play("idle-left-right")
 			anim.flip_h = last_direction.x < 0
 		else:
-			if last_direction.y > 0:
-				anim.play("idle-down")
-			else:
-				anim.play("idle-up")
+			anim.play("idle-down" if last_direction.y > 0 else "idle-up")
 	else:
 		if abs(dir.x) > abs(dir.y):
 			anim.play("run-left-right")
 			anim.flip_h = dir.x < 0
 		else:
-			if dir.y > 0:
-				anim.play("run-down")
-			else:
-				anim.play("run-up")
+			anim.play("run-down" if dir.y > 0 else "run-up")
 
 
 @rpc("any_peer", "unreliable")
 func sync_position(pos: Vector2):
 	if !is_multiplayer_authority():
-		global_position = pos
+		network_pos = pos
+
+
+@rpc("any_peer", "unreliable")
+func sync_direction(dir: Vector2, last_dir: Vector2):
+	if !is_multiplayer_authority():
+		last_direction = last_dir
+		_update_animation(dir)
--- test_scene.gd
+++ test_scene.gd
@@ -5,24 +5,45 @@ var PlayerScene = preload("res://player.tscn")
 
 func _ready():
 	var mp = get_tree().get_multiplayer()
-
 	mp.peer_connected.connect(_on_player_connected)
 	mp.peer_disconnected.connect(_on_player_disconnected)
 
-	# Spawn du joueur local
+	# Spawn LOCAL seulement
 	spawn_player(mp.get_unique_id())
 
 func _on_player_connected(peer_id):
+	var mp = get_tree().get_multiplayer()
+	var local_id = mp.get_unique_id()
+
+	# VERY IMPORTANT !!!
+	if peer_id == local_id:
+		print("IGNORED self connect:", peer_id)
+		return
+
+	# Avoid double spawn
+	if has_node(str(peer_id)):
+		print("Player already exists:", peer_id)
+		return
+
+	print("CONNECTED:", peer_id)
 	spawn_player(peer_id)
 
 func _on_player_disconnected(peer_id):
+	print("DISCONNECTED:", peer_id)
 	if has_node(str(peer_id)):
 		get_node(str(peer_id)).queue_free()
 
 func spawn_player(peer_id):
+	print("SPAWN PLAYER:", peer_id)
 	var p = PlayerScene.instantiate()
 	p.name = str(peer_id)
-	add_child(p)
 
-	p.global_position = spawn_point.global_position
 	p.set_multiplayer_authority(peer_id)
+
+	add_child(p)
+	var spawns = $SpawnPoints.get_children()
+	var spawn = spawns[randi() % spawns.size()]
+	p.global_position = spawn.global_position
+
+
+	print("SET AUTHORITY:", peer_id)
